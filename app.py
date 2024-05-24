2c5d64618e4252513e22487995fe6897bc806934
Ravi <99581144+ravigithubshankar@users.noreply.github.com>
2024-05-24 21:21:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,14 @@ def query_stabilitydiff(payload, headers):
     response = requests.post(API_URL, headers=headers, json=payload)
     return response.content
 
+# Check if the Hugging Face token is available in Streamlit Secrets
+if "hugging_face_token" not in st.secrets:
+    st.error("Hugging Face token is missing. Please add it to Streamlit Secrets.")
+    st.stop()
+
+# Access the Hugging Face token
+api_key = st.secrets["hugging_face_token"]
+
 with st.sidebar:
     st.title("💬 Chatbot - Text to Image Gen")
     st.caption("🚀 A Streamlit chatbot powered by Strom")
@@ -23,33 +31,21 @@ for message in st.session_state.messages:
 
 if prompt := st.chat_input():
 
-    if not isinstance(st.secrets.hugging_face_token, dict):
-        st.info("Your Hugging Face Token configuration seems incorrect. Please check your secrets setup.")
-        st.stop()
-    elif "api_key" not in st.secrets.hugging_face_token:
-        st.info("Please add your Hugging Face Token API key to continue.")
-        st.stop()
-
-
     # Input prompt
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
     # Query Stable Diffusion
-    headers = {"Authorization": f"Bearer {st.secrets.hugging_face_token.api_key}"}
+    headers = {"Authorization": f"Bearer {api_key}"}
     image_bytes = query_stabilitydiff({
         "inputs": prompt,
     }, headers)
 
     # Return Image
     image = Image.open(io.BytesIO(image_bytes))
-    msg = f'here is your image related to "{prompt}"'
+    msg = f'Here is your image related to "{prompt}"'
 
     # Show Result
     st.session_state.messages.append({"role": "assistant", "content": msg, "prompt": prompt, "image": image})
     st.chat_message("assistant").write(msg)
     st.chat_message("assistant").image(image, caption=prompt, use_column_width=True)
-
-
-
-
